9d54c8a2416a7e0e7fbf48536cb5584b6ffb41d1
HuiGong-dev <hellohuigong@gmail.com>
2023-04-26 21:51:31 +0200

    add logo in navbar

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import { Navbar } from '../../components/navbar';
 
 const PageContainer = styled.div`
   ${tw`
-flex
-flex-col
-w-full
-h-full
-items-center
-overflow-x-hidden
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
 `}
 `;
 
 export function HomePage() {
-  return <PageContainer>Hello Pakkkge</PageContainer>;
+  return (
+    <PageContainer>
+      <Navbar></Navbar>
+    </PageContainer>
+  );
 }
